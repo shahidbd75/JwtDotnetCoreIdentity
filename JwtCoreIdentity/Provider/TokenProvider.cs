dba7812369e2179cd8eb7f03d0dfbd56b06f50be
Shahid <abdul.baset@aci-bd.com>
2019-06-25 16:29:43 +0600

    Add Claims before add identity

--- JwtCoreIdentity/Provider/TokenProvider.cs
+++ JwtCoreIdentity/Provider/TokenProvider.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
+using System.Security.Claims;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Configuration;
@@ -23,11 +24,21 @@ namespace JwtCoreIdentity.Provider
             var audience = configuration["Jwt:Audience"];
             var expiry = DateTime.Now.AddMinutes(30);
 
+            var claims = new Claim[]
+            {
+                new Claim(JwtRegisteredClaimNames.Sub, "1"),
+                new Claim(JwtRegisteredClaimNames.UniqueName, "Shahid"),
+                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+                new Claim(JwtRegisteredClaimNames.Iat, DateTime.Now.ToString("d"))
+            };
+
+
+
             var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["Jwt:Key"]));
             var credentials = new SigningCredentials(securityKey,SecurityAlgorithms.HmacSha256);
 
             var token = new JwtSecurityToken(issuer: issuer, audience: audience, expires: expiry,
-                signingCredentials: credentials);
+                signingCredentials: credentials,claims: claims);
 
             var tokenHandler = new JwtSecurityTokenHandler();
 
